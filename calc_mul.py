0a957dc8dd2c68f19cca6a31e33c1b8dd5c85a14
SStoyohasi2025 <saito.sho.lx@tut.jp>
2025-02-04 20:28:06 +0900

    calc() function and test cases Update

--- calc_mul.py
+++ calc_mul.py
@@ -3,32 +3,20 @@
 import re
                 
 def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
-        
+	if not isinstance(A,int) or not isinstance(B,int):
+		return -1
+
+	if A<1 or A>999 or B<1 or B>999:
+		return -1
+
+	return A*B
                 
 def main ():
 	matchstring = ''
 	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+		A = input ('input A: ')
+		B = input ('input B: ')
+		print ('input A * input B = ', calc(A,B))
 
 if __name__ == '__main__':
 	main()
--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,36 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+	def test_narmal (self):
+		self.assertEqual (21, calc(3,7))
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+	def test_min (self):
+		self.assertEqual (1, calc(1,1))
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+	def test_max (self):
+		self.assertEqual (999*999, calc(999,999))
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+	def test_zero (self):
+		self.assertEqual (-1, calc(0,1))
+		self.assertEqual (-1, calc(1,0))
+		self.assertEqual (-1, calc(0,0))
 
+	def test_over (self):
+		self.assertEqual (-1, calc(1000,1))
+		self.assertEqual (-1, calc(1,1000))
+		self.assertEqual (-1, calc(1000,1000))
+
+	def test_str (self):
+		self.assertEqual (-1, calc("a",1))
+		self.assertEqual (-1, calc(1,"a"))
+		self.assertEqual (-1, calc("a","b"))
+
+	def test_float (self):
+		self.assertEqual (-1, calc(0.1,999))
+		self.assertEqual (-1, calc(999,0.1))
+		self.assertEqual (-1, calc(0.1,0.1))
+
+	def test_special_character (self):
+		self.assertEqual (-1, calc("@",1))
+		self.assertEqual (-1, calc(1,"@"))
+		self.assertEqual (-1, calc("@","@"))
